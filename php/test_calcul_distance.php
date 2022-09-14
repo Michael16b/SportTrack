46cddbb87f3002e263995091fd794954f464fd58
Evahhhh <eve-anneoffredi@orange.fr>
2022-09-14 22:04:07 +0200

    succès

--- php/test_calcul_distance.php
+++ php/test_calcul_distance.php
@@ -5,12 +5,15 @@ require_once 'CalculDistanceImpl.php';
 $classCalc = new CalculDistanceImpl();
 
 $parcours = [
+    ['latitude' => 47.644795, 'longitude' => -2.776605],
     ['latitude' => 47.646870, 'longitude' => -2.778911],
     ['latitude' => 47.646197, 'longitude' => -2.780220],
-    ['latitude' => 47.646197, 'longitude' => -2.780220],
     ['latitude' => 47.646992, 'longitude' => -2.781068],
-    
+    ['latitude' => 47.647867, 'longitude' => -2.781744],
+    ['latitude' => 47.648510, 'longitude' => -2.780145]
 ];
+
+
 
 $distTotale = $classCalc->calculDistanceTrajet($parcours);
 echo $distTotale;
