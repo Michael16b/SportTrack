fdcf3a266043f64563ceb4c9d71188aab1569d71
Michaël <michael.besily@gmail.com>
2022-09-14 21:33:49 +0200

    erreur parcours supprimé

--- php/test_calcul_distance.php
+++ php/test_calcul_distance.php
@@ -4,13 +4,13 @@ require_once 'CalculDistanceImpl.php';
 
 $classCalc = new CalculDistanceImpl();
 
-$etap1 = $classCalc->calculDistance2PointsGPS(47.644795,-2.776605,47.646870,-2.778911);
-$etap2 = $classCalc->calculDistance2PointsGPS(47.646870,-2.778911,47.646197,-2.780220);
-$etap3 = $classCalc->calculDistance2PointsGPS(47.646197,-2.780220,47.646992,-2.781068);
-$etap4 = $classCalc->calculDistance2PointsGPS(47.646992,-2.781068,47.647867,-2.781744);
-$etap5 = $classCalc->calculDistance2PointsGPS(47.647867,-2.781744,47.648510,-2.780145);
-
-$parcours = array($etap1,$etap2,$etap3,$etap4,$etap5);
+$parcours = [
+    ['latitude' => 47.646870, 'longitude' => -2.778911],
+    ['latitude' => 47.646197, 'longitude' => -2.780220],
+    ['latitude' => 47.646197, 'longitude' => -2.780220],
+    ['latitude' => 47.646992, 'longitude' => -2.781068],
+    
+];
 
 $distTotale = $classCalc->calculDistanceTrajet($parcours);
 echo $distTotale;
