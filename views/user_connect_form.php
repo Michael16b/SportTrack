84f8c81b3a49d01695c5b156ee3e2fc086595d4f
Michaël <michael.besily@gmail.com>
2022-10-02 17:33:41 +0200

    ajout connaissance de connexion

--- views/user_connect_form.php
+++ views/user_connect_form.php
@@ -1,5 +1,12 @@
 <?php include __ROOT__."/views/header.html"; ?>
-
+<?php
+if ($_SESSION) {
+    echo "<h5>ATTENTION, vous êtes déjà connecté en tant que : " . $_SESSION["surname"] . " " . $_SESSION["name"] . " </h5>";
+    echo "<h5>Vous allez être déconnecté si vous vous connectez à un autre compte<h5>";
+} else {
+  
+}
+?>
 <form action="/connect" method="post">
   <div>
       <label for="mail">Adresse e-mail :</label>
