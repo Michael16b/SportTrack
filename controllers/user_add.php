588d382e0ac1e98844793534c3fc361a90e8e353
Michaël <michael.besily@gmail.com>
2022-10-04 23:52:46 +0200

    début d'implémentation de getMail duplication

--- controllers/user_add.php
+++ controllers/user_add.php
@@ -10,20 +10,34 @@ class AddUserController extends Controller{
     }
 
     public function post($request){
-        $this->render('user_add_valid',['surname' => $request['surname'], 
-                                        'name' => $request['name'],
-                                        'date' => $request['date'],
-                                        'gender' => $request['gender'],
-                                        'size' => $request['size'],
-                                        'weight' => $request['weight'],
-                                        'mail' => $request['mail'],
-                                        'password' => $request['password']
-                                    ]);
+        $allUsers = UserDAO::getInstance()->findAll();
+        $mailExist = false;
+        foreach ($allUsers as $user) {
+            if ($user->getMail() == $request['mail']) {
+                $mailExist = true;
+            }
+        }
+        if (!$mailExist) {  
+            $this->render('user_add_valid',['surname' => $request['surname'], 
+                                            'name' => $request['name'],
+                                            'date' => $request['date'],
+                                            'gender' => $request['gender'],
+                                            'size' => $request['size'],
+                                            'weight' => $request['weight'],
+                                            'mail' => $request['mail'],
+                                            'password' => $request['password']
+                                        ]);
+
+        
 
 
-    $user = new User();
-    $user -> init($request['surname'],$request['name'],$request['date'],$request['gender'],$request['size'],$request['weight'],$request['mail'],$request['password']);
-    UserDAO::getInstance()->insert($user);  
+                            
+            $user = new User();
+            $user -> init($request['surname'],$request['name'],$request['date'],$request['gender'],$request['size'],$request['weight'],$request['mail'],$request['password']);
+            UserDAO::getInstance()->insert($user);
+            } else {
+                $this->render('error',['Le Mail existe déjà, veuillez choisir une autre adresse mail']);
+            }
     }
     
 }
