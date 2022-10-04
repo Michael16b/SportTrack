8fcfe87edc1d1bcae1de5f112c46676a9ce5f9ee
Michaël Besily <michael.besily@etu.univ-nantes.fr>
2022-10-05 01:14:17 +0200

    mis à jour CSS + reglement Fréquence Cardiaque

--- views/list_activities.php
+++ views/list_activities.php
@@ -8,24 +8,23 @@ if ($data != []) {
 
 <caption>Vos activités</caption>
 
-<tr> 
-    <th>Description</th> <th>Date</th> <th>Durée</th> <th>Distance</th> <th>Fréquence Cardiaque Minimum</th> 
-    <th>Fréquence Cardiaque Moyenne</th> <th>Fréquence Cardiaque Maximum</th>
+<tr align="center"> 
+    <th>| Description |</th> <th>| Date |</th> <th>| Durée |</th> <th>| Distance |</th> <th>| Fréquence Cardiaque Minimum |</th> 
+    <th>| Fréquence Cardiaque Moyenne |</th> <th>| Fréquence Cardiaque Maximum |</th>
 </tr>
 
 
 <?php
     foreach ($data as $activity) {
         ?>
-        
-        <tr> 
-            <td><?php echo($activity[0]) ?></td> 
+        <tr align="center"> 
+            
+            <td><?php echo($activity[0]) ?></td>
             <td><?php echo($activity[1]) ?></td>
             <td><?php echo($activity[2]) ?></td>
             <td><?php echo($activity[3]) ?></td>
             <td><?php echo($activity[4]) ?></td>
             <td><?php echo($activity[5]) ?></td>
-            <td><?php echo($activity[6]) ?></td>
         </tr>
         
         <?php
