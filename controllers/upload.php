c51692fbae40690e6beec63a8d156cc8638f791e
Michaël <michael.besily@gmail.com>
2022-10-06 23:01:57 +0200

    ajout du bloquage d'activité si celui-ci est supérieur à la date du jour

--- controllers/upload.php
+++ controllers/upload.php
@@ -23,16 +23,28 @@ class UploadActivityController extends Controller{
                     
                     $arrayData = array();
                     $arrayActivity = array();
-
+                    $dateTimeNow = new DateTime();
+                    $dateNow = $dateTimeNow->format('Y-m-d');
+                    $dateBlock = false;
                     if (is_countable($jsonData)) {
                         if (count($jsonData) > 0) {
                             foreach($jsonData as $key => $value) {
-                                if ($key == "activity") {
-                                    array_push($arrayActivity, $value['description'], $value['date']);
-                                } else if ($key == "data") {
-                                    foreach($value as $value2){
-                                        $arrayData[] =  [$value2["time"], $value2["cardio_frequency"], $value2["latitude"], $value2["longitude"], $value2["altitude"]];
+                                if ($dateBlock == false) {
+                                    if ($key == "activity") {
+                                        $date = date('Y-m-d', strtotime($value['date']));
+                                        $date = new DateTime($date);
+                                        if ($date > $dateNow) {
+                                            $this->render('error',["La date de l'activité ne peut pas être supérieure à la date du jour \n"]);
+                                            $dateBlock = true;
+                                            break;
+                                        } else {
+                                            array_push($arrayActivity, $value['description'], $value['date']);
                                     }
+                                    } else if ($key == "data" ) {
+                                        foreach($value as $value2){
+                                            $arrayData[] =  [$value2["time"], $value2["cardio_frequency"], $value2["latitude"], $value2["longitude"], $value2["altitude"]];
+                                        }
+                                }
                                 
                             } else {
                                 $this->render('error',["Erreur de format de fichier"]);
@@ -40,46 +52,47 @@ class UploadActivityController extends Controller{
                         }
                     
                         
-                        
-                        $startTime = $arrayData[0][0];
-                        $duration =  strtotime($arrayData[count($arrayData)-1][0]) - strtotime($startTime);
-                        $duration =  date('H:i:s', $duration);
-                        
+                        if ($dateBlock == false) {
+                            $startTime = $arrayData[0][0];
+                            $duration =  strtotime($arrayData[count($arrayData)-1][0]) - strtotime($startTime);
+                            $duration =  date('H:i:s', $duration);
+                            
 
-                        $minCardio = $arrayData[0][1];
-                        $avgCardio = $arrayData[0][1];
-                        $maxCardio = $arrayData[0][1];
-                        for ($i=1; $i < count($arrayData); $i++) { 
-                            if ($arrayData[$i][1] < $minCardio) {
-                                $minCardio = $arrayData[$i][1];
+                            $minCardio = $arrayData[0][1];
+                            $avgCardio = $arrayData[0][1];
+                            $maxCardio = $arrayData[0][1];
+                            for ($i=1; $i < count($arrayData); $i++) { 
+                                if ($arrayData[$i][1] < $minCardio) {
+                                    $minCardio = $arrayData[$i][1];
+                                }
+                                if ($arrayData[$i][1] > $maxCardio) {
+                                    $maxCardio = $arrayData[$i][1];
+                                }
+                                $avgCardio += $arrayData[$i][1];
                             }
-                            if ($arrayData[$i][1] > $maxCardio) {
-                                $maxCardio = $arrayData[$i][1];
+                            $avgCardio = $avgCardio / count($arrayData);
+                            
+
+                            
+                            $arrayDistance = array();
+                            for ($i=0; $i < count($arrayData); $i++) { 
+                                $arrayDistance[] = array("longitude" => $arrayData[$i][3], "latitude" => $arrayData[$i][2]);
                             }
-                            $avgCardio += $arrayData[$i][1];
-                        }
-                        $avgCardio = $avgCardio / count($arrayData);
-                        
+                            $classCalc = new CalculDistanceImpl();
+                            $distance = $classCalc->calculDistanceTrajet($arrayDistance);
+                            $activity = new Activity();
+                            $activity -> init($arrayActivity[0],$arrayActivity[1],$startTime,$duration,round($distance,2),$minCardio,$avgCardio,$maxCardio,$_SESSION['idUser']);
+                            ActivityDAO::getInstance()->insert($activity);
 
-                        
-                        $arrayDistance = array();
-                        for ($i=0; $i < count($arrayData); $i++) { 
-                            $arrayDistance[] = array("longitude" => $arrayData[$i][3], "latitude" => $arrayData[$i][2]);
+                            $activity = ActivityDAO::getInstance()->findActivity($activity);
+                            $idActivity = $activity[0] -> getIdActivity();
+                            $data = new Data();
+                            $arrayData = array_values($arrayData);
+                            $data -> init($startTime,$duration,$arrayData[0][3],$arrayData[0][2],$arrayData[0][4],$idActivity);
+                            
+                            ActivityEntryDAO::getInstance()->insert($data);
+                            $this->render('upload_activity_valid',[$arrayActivity]);
                         }
-                        $classCalc = new CalculDistanceImpl();
-                        $distance = $classCalc->calculDistanceTrajet($arrayDistance);
-                        $activity = new Activity();
-                        $activity -> init($arrayActivity[0],$arrayActivity[1],$startTime,$duration,round($distance,2),$minCardio,$avgCardio,$maxCardio,$_SESSION['idUser']);
-                        ActivityDAO::getInstance()->insert($activity);
-
-                        $activity = ActivityDAO::getInstance()->findActivity($activity);
-                        $idActivity = $activity[0] -> getIdActivity();
-                        $data = new Data();
-                        $arrayData = array_values($arrayData);
-                        $data -> init($startTime,$duration,$arrayData[0][3],$arrayData[0][2],$arrayData[0][4],$idActivity);
-                        
-                        ActivityEntryDAO::getInstance()->insert($data);
-                        $this->render('upload_activity_valid',[$arrayActivity]);
                     } else {
                         $this->render('error',["Erreur de format de fichier"]);
                     }
--- views/error.php
+++ views/error.php
@@ -3,6 +3,8 @@
 include VIEWS_DIR."/header.html";
 
 echo $data[0];
+echo "<br/>";
+echo "<a href='/'>Allez sur la page d'accueil</a>";
 
 include VIEWS_DIR."/footer.html";
 ?>
